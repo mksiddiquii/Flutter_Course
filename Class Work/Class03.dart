28da5e5e0197aeb3152a448613acf93141892f94
Muhmmad Kamran <mksiddiquii@gmail.com>
2026-02-22 16:21:33 +0500

    repo made

---
+++ Class Work/Class03.dart
@@ -0,0 +1,54 @@
+// All installation done
+// vs code
+// git and git hub
+// flutter download and ste the path
+// go to environment varibale > path >new copy bin folder path.
+
+import 'dart:io';
+
+void main() {
+/*   print("hello World ");
+  int english = 90;
+  int math = 85;
+  int urdu = 90;
+  int islamit = 82;
+  int science = 75;
+
+  int totalMarks = (english + math + urdu + islamit + science);
+  double percentage = (totalMarks / 500) * 100;
+  print(percentage.floorToDouble());
+
+  if (percentage > 90) {
+    print("A+ garde");
+  } else if (percentage > 80) {
+    print("A  garde");
+  } else if (percentage > 70) {
+    print("B garde");
+  } else if (percentage > 60) {
+    print("C  garde");
+  } else {
+    print("fail");
+  } */
+  /* List<int> number = [10, 2, 9, 11, 16, 14, 19, 19];
+  print(number);
+  print(number.length);
+  print(number[2]);
+  print(number[number.length - 1]);
+  print(number.first);
+  print(number.last);
+  print(number.elementAt(3)); */
+
+  List<String> fruitsName = ["Mango", "Orange", "Banana", "Kiwi", "Grapes"];
+  print(fruitsName);
+  print(fruitsName.length);
+  print(fruitsName[2]);
+  print(fruitsName[fruitsName.length - 1]);
+  print(fruitsName.first);
+  print(fruitsName.last);
+  print(fruitsName.elementAt(3));
+
+
+
+
+
+}
