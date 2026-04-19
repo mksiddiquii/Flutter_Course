adf9316ef62ecbb2fb0fa19527bc80f80df4d6c8
Muhmmad Kamran <mksiddiquii@gmail.com>
2026-04-19 16:29:19 +0500

    Flutter_first_app

--- Project_Practice/my_first_project/myfirstapp/lib/screens/home_view.dart
+++ Project_Practice/my_first_project/myfirstapp/lib/screens/home_view.dart
@@ -16,7 +16,14 @@ class HomeView extends StatelessWidget {
         child: Column(
           
           children: [
-            Container(height: 200, width: 2000, color: Colors.lime,),
+            Container(height: 200, width: 2000, decoration: BoxDecoration(
+              color: Colors.black26,
+              shape: BoxShape.circle,
+            ),
+            child: Center(
+              child: Text("Container No.1",style: TextStyle(color: Colors.yellow,fontSize: 28),),
+              
+            ),),
             Container(height: 200, width: 200, color: Colors.redAccent),
             Container(height: 200, width: 200, color: Colors.greenAccent),
             Container(height: 200, width: 200, color: Colors.blueAccent),
