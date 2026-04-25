3c14e6babdec4a8c58d8a4a0a1105d9382e6e38d
Muhmmad Kamran <mksiddiquii@gmail.com>
2026-04-25 20:42:11 +0500

    uodate project name

--- Project_Practice/my_first_project/myfirstapp/lib/main.dart
+++ Project_Practice/my_first_project/myfirstapp/lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:myfirstapp/screens/home_view.dart';
 
 void main() {
-  runApp(const Myapp());
+  runApp(const MyApp());
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
